3716c6b073f3b06ec87ad9f0c3777c2aa8a75db7
DaniParkHoJin <parkhojin@naver.com>
2024-07-09 18:24:20 +0900

    7/9 수업 후

--- member/javascript/join.js
+++ member/javascript/join.js
@@ -20,7 +20,7 @@ function zipcode_openwin() {
     new daum.Postcode({
         oncomplete: function (data) {
             document.getElementById('zipcode').value = data.zonecode;
-            document.getElementById('address1').value = data.address;
+            document.getElementById('id_address1').value = data.address;
         }
     }).open();
 }
@@ -29,11 +29,11 @@ function checkForm(regForm) {
     let r1 = /[0123456789]/g;
     let r2 = /[^a-zA-Z0-9]/g;
     let r3 = "~!@#$%^&*()_+=|\/?:\"'<>,.()[]{};"
-    unname = regForm.name.value;
+    unname = regForm.name_name.value;
     b = false;
-    if (name.replace(/\s/g, "") == "") {
-        alert("이름을 입력하세요.");
-        regForm.name.focus();
+    if (unname.replace(/\s/g, "") == "") {
+        alert("올바른 이름을 입력하세요.");
+        regForm.name_name.focus();
         return false;
     }
     for (let i = 0; i < unname.lang; i++) {
@@ -45,21 +45,34 @@ function checkForm(regForm) {
     }
     if (b) {
         alert("이름은 문자만 입력하셔야 합니다.");
-        regForm.name = "";
-        regForm.name.focus();
+        regForm.name_name = "";
+        regForm.name_name.focus();
         return false;
     }
-    id = regForm.userid.value;
+    id = regForm.name_userid.value;
     if (id.replace(/\s/g, "") == "" || id.search(r2) >= 0) {
-        alert("아이디가 입력되지 않았거나 아이디는 영문자와 숫자로 구성되어야 합니다.");
-        regForm.userid.value="";
-        regForm.userid.focus();
+        alert("아이디가 입력되지 않았거나 아이디는 영문자로 구성되어야 합니다.");
+        regForm.name_userid.value="";
+        regForm.name_userid.focus();
         return false;
     }
     if (id.substr(0,1).match(r1)){
         alert("아이디의 첫 글자에는 숫자가 올 수 없습니다.");
-        regForm.userid.value="";
-        regForm.userid.focus();
+        regForm.name_userid.value="";
+        regForm.name_userid.focus();
         return false;
     }
+    pw = regForm.name_password.value;
+    if(pw.replace(/\s/g, "") == "" || pw.search(r2) >= 0) {
+        alert("비밀번호는 영문자와 숫자로 구성되어야 합니다.");
+        regForm.name_password.value="";
+        regForm.name_password.focus();
+    }
+
+    if(pw != regForm.name_repassword){
+        alert("비밀번호와 비밀번호 확인이 다릅니다. 다시 입력해주세요.");
+        regForm.name_repassword.value="";
+        regForm.name_repassword.focus();
+    }
 }
+
